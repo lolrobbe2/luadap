06833b12191c9590bc30e50d1b856ab41ca76608
Beernaert Robbe <robbe.beernaert@student.howest.be>
2025-01-23 19:08:05 +0100

    allot of hard work

--- projects/sandbox.lua
+++ projects/sandbox.lua
@@ -1,5 +1,5 @@
 project "sandbox"
-    location "projects/sandbox"
+    location "../src/sandbox"
     kind "ConsoleApp"
     language "C++"
     cppdialect "C++17"
@@ -9,12 +9,14 @@ project "sandbox"
     files { "../src/sandbox/**.cpp", "../src/sandbox/**.h","../src/sandbox/**.lua" }
 
     includedirs {
-        "../external/lua/src",          -- Include directory for Lua
+        "../external/lua/src", -- Include directory for Lua
         "../external/luasocket/src" -- Include directory for LuaSocket
     }
-
     links { "LuaCore", "LuaSocket", "ws2_32" } -- Link with LuaCore and LuaSocket
+    
 
+    buildoptions { "/EHsc", "/utf-8", "/D _UNICODE", "/D UNICODE" }  -- Add flags for Debug configuration    
+    
     filter "system:windows"
         systemversion "latest"
         defines {"SANDBOX_WINDOWS", "WIN32", "_WINDOWS", "_CRT_SECURE_NO_WARNINGS", "LUASOCKET_WINDOWS", "LUASOCKET_EXPORTS","LUA_STATICLIB" }
@@ -22,6 +24,7 @@ project "sandbox"
     filter "configurations:Debug"
         defines { "DEBUG" }
         symbols "On"
+        debugdir "../src/sandbox"
 
     filter "configurations:Release"
         defines { "NDEBUG" }
---
+++ src/sandbox/main.lua
@@ -0,0 +1,10 @@
+local luadap = require("luadap")
+
+
+-- Start the Luadap server
+
+-- Main loop to accept client connections and handle data
+
+
+
+luadap.start()
